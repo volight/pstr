b9d45093a8bf25184ea87fe14f8977a4b540eaf2
Package <u2a5f@outlook.com>
2020-10-24 19:01:29 +0800

    0.2.2

--- src/pool.rs
+++ src/pool.rs
@@ -1,13 +1,11 @@
 //! The String Intern Pool  
 
-use std::sync::Arc;
-
 use dashmap::DashSet;
 use once_cell::sync::Lazy;
 
 use crate::prc::Prc;
 
-static POOL: Lazy<Arc<DashSet<Prc<str>>>> = Lazy::new(|| Arc::new(DashSet::new()));
+static POOL: Lazy<DashSet<Prc<str>>> = Lazy::new(|| DashSet::new());
 
 #[derive(Debug, Clone, Eq, Ord, PartialOrd)]
 pub(crate) struct Handle(Prc<str>);
@@ -15,30 +13,28 @@ pub(crate) struct Handle(Prc<str>);
 impl Handle {
     #[inline]
     pub fn new(slice: &str) -> Self {
-        let pool = POOL.clone();
-        match pool.get(slice).map(|v| v.key().clone()) {
+        match POOL.get(slice).map(|v| v.key().clone()) {
             Some(v) => Self(v),
             None => {
                 let prc = Prc::from_box(Box::from(slice));
-                if pool.insert(Clone::clone(&prc)) {
+                if POOL.insert(Clone::clone(&prc)) {
                     Self(prc)
                 } else {
-                    Self(pool.get(prc.as_ref()).unwrap().key().clone())
+                    Self(POOL.get(prc.as_ref()).unwrap().key().clone())
                 }
             }
         }
     }
     #[inline]
     pub fn from_box(slice: Box<str>) -> Self {
-        let pool = POOL.clone();
-        match pool.get(slice.as_ref()).map(|v| v.key().clone()) {
+        match POOL.get(slice.as_ref()).map(|v| v.key().clone()) {
             Some(v) => Self(v),
             None => {
                 let prc = Prc::from_box(slice);
-                if pool.insert(prc.clone()) {
+                if POOL.insert(prc.clone()) {
                     Self(prc)
                 } else {
-                    Self(pool.get(prc.as_ref()).unwrap().key().clone())
+                    Self(POOL.get(prc.as_ref()).unwrap().key().clone())
                 }
             }
         }
@@ -58,8 +54,7 @@ impl PartialEq for Handle {
 
 /// Delete all interning string with reference count == 1 in the pool
 pub fn collect_garbage() {
-    let pool = POOL.clone();
-    pool.retain(|prc| Prc::<str>::strong_count(prc) > 1)
+    POOL.retain(|prc| Prc::<str>::strong_count(prc) > 1)
 }
 
 #[cfg(test)]
@@ -93,17 +88,16 @@ mod tests {
     #[test]
     #[ignore]
     fn test_pool_gc() {
-        let pool = POOL.clone();
-        assert_eq!(pool.len(), 0);
+        assert_eq!(POOL.len(), 0);
         Handle::new("asd");
-        assert_eq!(pool.len(), 1);
+        assert_eq!(POOL.len(), 1);
         let h = Handle::new("123");
-        assert_eq!(pool.len(), 2);
+        assert_eq!(POOL.len(), 2);
         collect_garbage();
-        assert_eq!(pool.len(), 1);
+        assert_eq!(POOL.len(), 1);
         drop(h);
-        assert_eq!(pool.len(), 1);
+        assert_eq!(POOL.len(), 1);
         collect_garbage();
-        assert_eq!(pool.len(), 0);
+        assert_eq!(POOL.len(), 0);
     }
 }
